a2c4b6a3030c07891192211e92b673419ab4a7c5
vinicius.diniz21 <vinicius.diniz444@gmail.com>
2026-02-07 14:33:31 -0300

    allw host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+
+    allowedHosts: [
+      'viniciusdinizdev.com.br',
+      'www.viniciusdinizdev.com.br',
+      'localhost'
+    ],
+
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
+        secure: false,
       }
     }
   }
